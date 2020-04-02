47263d3085bb2e543f90b008e40574010358f0e4
pedro.rodriguez <pedro.rodriguez@sngular.com>
2020-04-02 15:41:04 +0200

    Despliegue de un servicio java bajo Kubernetes

---
+++ dkh_service1/src/main/java/dkh/demo/service1/DkhService1Application.java
@@ -0,0 +1,13 @@
+package dkh.demo.service1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkhService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DkhService1Application.class, args);
+	}
+
+}
---
+++ dkh_service1/src/test/java/dkh/demo/service1/DkhService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package dkh.demo.service1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DkhService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
